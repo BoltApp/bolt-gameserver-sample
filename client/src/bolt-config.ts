b86d76e5172f27577164727226aeef9a54804f55
Alan Thai <athai@bolt.com>
2025-07-11 09:52:39 -0400

    feat: update bce links

--- client/src/bolt-config.ts
+++ client/src/bolt-config.ts
@@ -1,10 +1,10 @@
 export const BoltConfig = {
   checkoutLinks: {
-    STARTER: 'https://gregs-guava-myshopify.c-staging.bolt.com/c?u=7oLxSjeYAcfTFpKsK2o43r&publishable_key=zQVb4QDUzwJD.GOxcEQV1ZNbW.bb17ba147d91e23de2647182d1381b60b281a2cd47092642a2fa214229cc43de',
-    BRONZE: 'https://gregs-guava-myshopify.c-staging.bolt.com/c?u=7oLxSjeYAcfTFpKsK2o43r&publishable_key=zQVb4QDUzwJD.GOxcEQV1ZNbW.bb17ba147d91e23de2647182d1381b60b281a2cd47092642a2fa214229cc43de',
-    SILVER: 'https://gregs-guava-myshopify.c-staging.bolt.com/c?u=7oLxSjeYAcfTFpKsK2o43r&publishable_key=zQVb4QDUzwJD.GOxcEQV1ZNbW.bb17ba147d91e23de2647182d1381b60b281a2cd47092642a2fa214229cc43de',
-    GOLD: 'https://gregs-guava-myshopify.c-staging.bolt.com/c?u=7oLxSjeYAcfTFpKsK2o43r&publishable_key=zQVb4QDUzwJD.GOxcEQV1ZNbW.bb17ba147d91e23de2647182d1381b60b281a2cd47092642a2fa214229cc43de',
-    PLATINUM: 'https://gregs-guava-myshopify.c-staging.bolt.com/c?u=7oLxSjeYAcfTFpKsK2o43r&publishable_key=zQVb4QDUzwJD.GOxcEQV1ZNbW.bb17ba147d91e23de2647182d1381b60b281a2cd47092642a2fa214229cc43de',
-    DIAMOND: 'https://gregs-guava-myshopify.c-staging.bolt.com/c?u=7oLxSjeYAcfTFpKsK2o43r&publishable_key=zQVb4QDUzwJD.GOxcEQV1ZNbW.bb17ba147d91e23de2647182d1381b60b281a2cd47092642a2fa214229cc43de',
+    STARTER: 'https://knights-of-valor-bolt.c-staging.bolt.com/c?u=GUTxRpognVPZwhBNE5ZWnS',
+    BRONZE: 'https://knights-of-valor-bolt.c-staging.bolt.com/c?u=kFwd3eeuAGcviMT2NAZqH4',
+    SILVER: 'https://knights-of-valor-bolt.c-staging.bolt.com/c?u=g9mR2RiizPD8TdLJkVr5n8',
+    GOLD: 'https://knights-of-valor-bolt.c-staging.bolt.com/c?u=KhCBitpr5ZC7TbRLQAQ2ds',
+    PLATINUM: 'https://knights-of-valor-bolt.c-staging.bolt.com/c?u=MTFHeP3dcZL8eXxHumjPZ2',
+    DIAMOND: 'https://knights-of-valor-bolt.c-staging.bolt.com/c?u=ioLKpzww9BA8PeoGFiCxwy',
   }
 }
