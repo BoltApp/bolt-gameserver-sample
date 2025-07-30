9c2922d20f56148b8ef784d3e7024714990de88d
Winona Schroeer-Smith <winona@bolt.com>
2025-07-30 12:01:54 -0700

    test whitespace

--- server/src/routes/bolt.ts
+++ server/src/routes/bolt.ts
@@ -37,6 +37,7 @@ router.post('/webhook', verifySignature, async (req, res) => {
     console.error(`Error handling Bolt webhook on object ${input.object}`, error)
     const response: ApiResponse<null> = { success: false, error: `Webhook failed` }
     res.status(500).json(response)
+
   }
 })
 
