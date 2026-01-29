d607a222d8de6ebfc86289ea8bec9f8f94d6fded
aweislow <166541686+aweislow@users.noreply.github.com>
2026-01-29 08:56:02 -0800

    Zappy Bird Gameplay Loop (#42)

---
+++ client/src/zappy_bird/runtime/bootstrap.ts
@@ -0,0 +1,12 @@
+import type { ZappyBirdFB } from '../types';
+
+export function ensureZappyGlobals(): { FB: ZappyBirdFB; GAME_CONFIG: { spaceshipEnabled: boolean; voltageBoost: boolean } } {
+  const w = window;
+  if (!w.FB) {
+    w.FB = {} as ZappyBirdFB;
+  }
+  if (!w.GAME_CONFIG) {
+    w.GAME_CONFIG = { spaceshipEnabled: false, voltageBoost: false };
+  }
+  return { FB: w.FB as ZappyBirdFB, GAME_CONFIG: w.GAME_CONFIG };
+}
