1db058a13d3a29533264afdd970e4321ef785a83
Alan Thai <winged.cloudchaser@gmail.com>
2026-03-13 15:25:20 -0400

    update bolt-js@0.2.9 (#46)
    * update bolt-js@0.2.9
    
    * PR fixes

--- client/src/components/DemoTabs.tsx
+++ client/src/components/DemoTabs.tsx
@@ -2,14 +2,12 @@ export function DemoTabs() {
   return (
     <div className="demo-top-bar">
       <div className="demo-tabs-container">
-        <button
-          className="demo-tab"
-          onClick={() =>
-            (window.location.href = "http://demo.staging-bolt.com/")
-          }>
+        <a className="demo-tab" href="http://demo.staging-bolt.com/">
           Standard Demo
-        </button>
-        <button className="demo-tab demo-tab-active">Gaming Demo</button>
+        </a>
+        <a className="demo-tab demo-tab-active" href="#">
+          Gaming Demo
+        </a>
       </div>
     </div>
   );
